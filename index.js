d19b02a9f1122b2cac44e1aa39581a07655db14c
Jaden <jaden.franks@gmail.com>
2024-06-25 18:40:51 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching (collection, string){ 
+ return collection.filter(function (name){
+    return name.toUpperCase() == string.toUpperCase()
+  }) 
+}
+
+function fuzzyMatch (collection , string){  
+  return  collection.filter(function(name){
+    return name.slice(0, 2) === string
+ })
+}
+
+function matchName(collection, string){
+  return collection.filter(function (driverObject){
+    return driverObject.name === string 
+  })
+}
